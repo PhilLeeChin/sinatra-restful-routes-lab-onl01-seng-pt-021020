70684bea6c59702385f2c0360bdb12e20f064f7e
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-08 19:32:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
-    redirect to "/recipes/#{@recipe.id}"    end
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id' do
